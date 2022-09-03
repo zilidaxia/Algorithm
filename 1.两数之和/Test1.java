d212a0b36c0078c0b0ea798f7b2deff462efb864
myname <2214736768@qq.com>
2022-09-03 14:38:46 +0800

    Initial commit

---
+++ 1.两数之和/Test1.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+
+/*
+ * 1. 两数之和
+ * 
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+ * 
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 
+ * 你可以按任意顺序返回答案。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 
+ * 示例 3：
+ * 
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
+
+/**
+ * Test1
+ */
+public class Test1 {
+
+    /**
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[] { hashtable.get(target - nums[i]), i };
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[] { 0 };
+    }
+
+    // 方法2
+    public int[] twoSum2(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[0];
+    }
+}
---
+++ 2.两数相加/Test.java
@@ -0,0 +1,67 @@
+/* 2. 两数相加
+
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+ 
+
+示例 1：
+
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+
+示例 2：
+
+输入：l1 = [0], l2 = [0]
+输出：[0]
+
+示例 3：
+
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1] */
+
+public class Test {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode h = new ListNode(0);
+        ListNode a = h;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = l1 != null ? l1.val : 0;
+
+            int v2 = l2 != null ? l2.val : 0;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            a.next = new ListNode(sum % 10);
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+            a = a.next;
+        }
+        if (carry == 1)
+            a.next = new ListNode(carry);
+        return h.next;
+
+    }
+}
