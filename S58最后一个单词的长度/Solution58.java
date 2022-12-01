ca88bb20a17dd586ca0e975daaee6bc9966bfa55
myname <2214736768@qq.com>
2022-12-01 09:23:35 +0800

    ez

---
+++ S58最后一个单词的长度/Solution58.java
@@ -0,0 +1,58 @@
+package S58最后一个单词的长度;
+
+//给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+//
+// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "Hello World"
+//输出：5
+//解释：最后一个单词是“World”，长度为5。
+//
+//
+// 示例 2：
+//
+//
+//输入：s = "   fly me   to   the moon  "
+//输出：4
+//解释：最后一个单词是“moon”，长度为4。
+//
+//
+// 示例 3：
+//
+//
+//输入：s = "luffy is still joyboy"
+//输出：6
+//解释：最后一个单词是长度为6的“joyboy”。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 10⁴
+// s 仅有英文字母和空格 ' ' 组成
+// s 中至少存在一个单词
+//
+//
+// Related Topics 字符串
+// 👍 522 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution58 {
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') end--;
+        if (end < 0) return 0;
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') start--;
+        return end - start;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ S67二进制求和/Solution67.java
@@ -0,0 +1,54 @@
+package S67二进制求和;
+
+//给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入:a = "11", b = "1"
+//输出："100"
+//
+// 示例 2：
+//
+//
+//输入：a = "1010", b = "1011"
+//输出："10101"
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= a.length, b.length <= 10⁴
+// a 和 b 仅由字符 '0' 或 '1' 组成
+// 字符串如果不是 "0" ，就不含前导零
+//
+//
+// Related Topics 位运算 数学 字符串 模拟
+// 👍 924 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution67 {
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
